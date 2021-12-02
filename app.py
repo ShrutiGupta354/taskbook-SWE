35a924c37b13bc9b1f2d6bfb702391db60c2f098
Greg DeLozier <gregdelozier@gmail.com>
2021-12-02 08:13:35 -0500

    Bootstrap 5 changes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 # flask web objects
 from flask import Flask
 from flask import render_template
+from flask import request
 
 # the base Flask object
 app = Flask(__name__)
@@ -16,6 +17,10 @@ app = Flask(__name__)
 @app.get('/tasks')
 def tasks():
     return render_template("tasks.html") 
+
+@app.get('/tasks-w3')
+def tasks_w3():
+    return render_template("tasks-w3.html") 
 
 @app.get('/login')
 def login():
@@ -46,13 +51,14 @@ def get_tasks():
 def create_task():
     'create a new task in the database'
     try:
-        data = request.json
+        data = request.get_json()
         for key in data.keys():
             assert key in ["description","list"], f"Illegal key '{key}'"
         assert type(data['description']) is str, "Description is not a string."
         assert len(data['description'].strip()) > 0, "Description is length zero."
         assert data['list'] in ["today","tomorrow"], "List must be 'today' or 'tomorrow'"
     except Exception as e:
+        print(400, str(e))
         return ("400 Bad Request:"+str(e), 400)
     try:
         task_table = taskbook_db.get_table('task')
@@ -63,6 +69,7 @@ def create_task():
             "completed":False
         })
     except Exception as e:
+        print(409, str(e))
         return ("409 Bad Request:"+str(e), 409)
     # return Success
     return {'status':200, 'success': True}
@@ -71,18 +78,19 @@ def create_task():
 def update_task():
     'update properties of an existing task in the database'
     try:
-        data = request.json
+        data = request.get_json()
         for key in data.keys():
             assert key in ["id","description","completed","list"], f"Illegal key '{key}'"
         assert type(data['id']) is int, f"id '{id}' is not int"
-        if "description" in request:
+        if "description" in data:
             assert type(data['description']) is str, "Description is not a string."
             assert len(data['description'].strip()) > 0, "Description is length zero."
-        if "completed" in request:
+        if "completed" in data:
             assert type(data['completed']) is bool, "Completed is not a bool."
-        if "list" in request:
+        if "list" in data:
             assert data['list'] in ["today","tomorrow"], "List must be 'today' or 'tomorrow'"
     except Exception as e:
+        print(400, str(e))
         return ("400 Bad Request:"+str(e), 400)
     if 'list' in data: 
         data['time'] = time.time()
@@ -90,6 +98,7 @@ def update_task():
         task_table = taskbook_db.get_table('task')
         task_table.update(row=data, keys=['id'])
     except Exception as e:
+        print(409, str(e))
         return ("409 Bad Request:"+str(e), 409)
     # return Success
     return {'status':200, 'success': True}
@@ -98,14 +107,16 @@ def update_task():
 def delete_task():
     'delete an existing task in the database'
     try:
-        data = request.json
+        data = request.get_json()
         assert type(data['id']) is int, f"id '{id}' is not int"
     except Exception as e:
+        print(400, str(e))
         return ("400 Bad Request:"+str(e), 400)
     try:
         task_table = taskbook_db.get_table('task')
         task_table.delete(id=data['id'])
     except Exception as e:
+        print(409, str(e))
         return ("409 Bad Request:"+str(e), 409)
     # return Success
     return {'status':200, 'success': True}
