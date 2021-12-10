121e9933106fab5d20bd4a78b5eeb80563871e98
kwirth0 <kwirth3160@gmail.com>
2021-12-10 15:23:05 -0500

    Created user table

--- setup.py
+++ setup.py
@@ -5,6 +5,19 @@ if __name__ == "__main__":
     task_table = taskbook_db.get_table('task')
     task_table.drop()
     task_table = taskbook_db.create_table('task')
+    task_table.insert_many([
+        # yyyy-mm-dd is what the datepicker gives so I had to change the date format. We have to keep consistent
+        {"date":"2022-01-15", "description":"Do something useful", "completed":True},
+        {"date":"2022-01-15", "description":"Do something fantastic", "completed":False},
+        {"date":"2022-01-15", "description":"Do something remarkable",  "completed":False},
+        {"date":"2022-01-15", "description":"Do something unusual",  "completed":True}
+    ])
     # this will have user's credentials for login
     user_cred_table = taskbook_db.create_table('user_cred')
+    user_cred_table.drop()
+    user_cred_table = taskbook_db.create_table('user_cred')
+    user_cred_table.insert_many([
+        {"email": "default@gmail.com", "username": "default", "password": "password", "view": "calendar"},
+        {"email": "admin@gmail.com", "username": "admin", "password": "admin", "view": "calendar"}
+    ])
      
