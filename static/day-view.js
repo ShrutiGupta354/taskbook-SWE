85eaa64b27c099d91c21c7ed9af80b93a62a4c62
Sagar Subedi <sagarsubediofficial@gmail.com>
2022-02-03 16:06:57 -0500

    just worked on /tasks front-end

--- static/day-view.js
+++ static/day-view.js
@@ -7,23 +7,46 @@ const numbers = ["00",
 "31"
 ];
 
+// since we are repeating code:
+function makeDescriptionHTML(task,taskDesc){
+    taskDesc += `<div class="toast show mb-3 mt-3" role="alert" aria-live="assertive" aria-atomic="true" style="cursor:pointer" onclick="window.location.href='/tasks'">`;
+        taskDesc += `<div class="toast-header justify-content-between">`;
+            // for now I have added the blue sun as the icon but that will change depending on the tag (low, med, high)
+            taskDesc += `<strong><i class="bi bi-brightness-high-fill" style="color:blue"></i></strong>`;
+            taskDesc += `<small class="text-muted">${task.date} | ${task.time}</small>`;
+        taskDesc += `</div>`;
+        taskDesc += `<div class="toast-body ${task.completed ? "completed" : "" }">`;
+            taskDesc += task.description;
+        taskDesc += `</div>`;
+    taskDesc += `</div>`;
+    return taskDesc;
+}
+
 
 //Function to display tasks in day view
 function displayDayTasks(key){
     let taskDesc = "";
-
+    let count = 0;
     //fetches tasks
     api_get_tasks(function(result){
         for(const task of result.tasks){
             //if task matches current day, display it
             if(task.date == key){
-                taskDesc += `<p class="taskdesc`;
-                if(task.completed) taskDesc += ` completed`;
-                taskDesc += `">` + task.description + `</p>`;
+                taskDesc = makeDescriptionHTML(task,taskDesc);
+                document.getElementById("toast-container-today").innerHTML = taskDesc;
+                count++;
             }
         }
-        //once all tasks are in the taskDesc, add them to list
-        document.getElementById("today-tasks").innerHTML = taskDesc;
+        if(count==0){
+            let task = {
+                date:"-",
+                time:"-",
+                description:"<h2 class='ms-3'>No tasks for today!<h2>",
+            }
+            taskDesc = makeDescriptionHTML(task,taskDesc);
+            document.getElementById("toast-container-today").innerHTML =taskDesc;
+        }
+
     });
 }
 
@@ -39,11 +62,18 @@ function displayNextTasks(key){
             if(count >= 10) break;
             if(task.date > key){
                 count++;
-                taskDesc += `<p class="taskdesc`;
-                if(task.completed) taskDesc += ` completed`;
-                taskDesc += `">` + task.description + `</p>`;
+                taskDesc = makeDescriptionHTML(task,taskDesc);
+                document.getElementById("toast-container-upcoming").innerHTML = taskDesc;
             }
         }
-        document.getElementById("upcoming-tasks").innerHTML = taskDesc;
+        if(count==0){
+            let task = {
+                date:"-",
+                time:"-",
+                description:"<h2 class='ms-3'>No upcoming tasks!<h2>",
+            }
+            taskDesc = makeDescriptionHTML(task,taskDesc);
+            document.getElementById("toast-container-upcoming").innerHTML =taskDesc;
+        }
     });
 }
